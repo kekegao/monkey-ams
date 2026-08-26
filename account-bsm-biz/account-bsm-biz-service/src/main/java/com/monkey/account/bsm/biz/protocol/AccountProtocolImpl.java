c809158ffe19e4cc69227e24e328aa08fd98e9a5
kekegao <872182090@qq.com>
2026-08-26 10:09:05 +0800

    gkk

--- account-bsm-biz/account-bsm-biz-service/src/main/java/com/monkey/account/bsm/biz/protocol/AccountProtocolImpl.java
+++ account-bsm-biz/account-bsm-biz-service/src/main/java/com/monkey/account/bsm/biz/protocol/AccountProtocolImpl.java
@@ -1,6 +1,7 @@
 package com.monkey.account.bsm.biz.protocol;
 
 import com.monkey.account.bsm.biz.api.AccountProtocol;
+import com.monkey.ams.common.response.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
@@ -9,5 +10,8 @@ import org.apache.dubbo.config.annotation.DubboService;
 public class AccountProtocolImpl implements AccountProtocol {
 
 
-
+    @Override
+    public Result openAccount() {
+        return null;
+    }
 }
