756041d3d9b239bec289c26a9a8b0fdb7e2ce001
kekegao <872182090@qq.com>
2026-08-26 10:26:41 +0800

    chore: 清理缓存，修复 .gitignore

--- user-bsm-biz/user-bsm-biz-service/src/main/java/com/monkey/user/bsm/biz/service/impl/UserServiceImpl.java
+++ user-bsm-biz/user-bsm-biz-service/src/main/java/com/monkey/user/bsm/biz/service/impl/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.monkey.user.bsm.biz.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.baomidou.mybatisplus.spring.service.impl.ServiceImpl;
 import com.monkey.user.bsm.biz.entity.UserEntity;
 import com.monkey.user.bsm.biz.mapper.UserMapper;
 import com.monkey.user.bsm.biz.service.inf.UserService;
