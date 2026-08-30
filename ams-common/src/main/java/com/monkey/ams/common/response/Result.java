4eaee0069c76f89b4c652ed6dbb3e0d9f2387306
kekegao <872182090@qq.com>
2026-08-30 20:18:29 +0800

    gkk 登录/注册

--- ams-common/src/main/java/com/monkey/ams/common/response/Result.java
+++ ams-common/src/main/java/com/monkey/ams/common/response/Result.java
@@ -14,7 +14,7 @@ public class Result<T>  implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-    private boolean success;
+    private Boolean success;
     private String code;
     private String message;
     private T data;
