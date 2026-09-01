7bab13c5cfae625c8b1a427b26c3e251a8c29d41
kekegao <872182090@qq.com>
2026-09-01 20:12:19 +0800

    gkk 登录/注册

---
+++ settlement-bsm-biz/settlement-bsm-biz-service/src/main/java/com/monkey/settlement/bsm/biz/service/SettlementBsmBizServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monkey.settlement.bsm.biz.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementBsmBizServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementBsmBizServiceApplication.class, args);
+    }
+
+}
---
+++ settlement-bsm-biz/settlement-bsm-biz-service/src/test/java/com/monkey/settlement/bsm/biz/service/SettlementBsmBizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkey.settlement.bsm.biz.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementBsmBizServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
