195101bcb5e10fa2d283229b284f26848e5a437e
kekegao <872182090@qq.com>
2026-09-05 19:10:49 +0800

    gkk 登录/注册

--- ams-app/src/main/java/com/monkey/ams/app/controller/pps/AccountController.java
+++ ams-app/src/main/java/com/monkey/ams/app/controller/pps/AccountController.java
@@ -1,6 +1,8 @@
 package com.monkey.ams.app.controller.pps;
 
+import com.monkey.account.bsm.biz.api.AccountProtocol;
 import com.monkey.account.bsm.biz.api.AccountRechargeProtocol;
+import com.monkey.account.bsm.biz.dto.AccountDto;
 import com.monkey.account.bsm.biz.request.RechargeAccountRequest;
 import com.monkey.ams.app.controller.BaseController;
 import com.monkey.ams.common.response.Result;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 账户充值
+ * 智运宝账户：充值 / 余额查询
  */
 @RestController
 @RequestMapping("/account")
@@ -19,6 +21,9 @@ public class AccountController extends BaseController {
 
     @DubboReference
     private AccountRechargeProtocol accountRechargeProtocol;
+
+    @DubboReference
+    private AccountProtocol accountProtocol;
 
     /**
      * 充值
@@ -29,6 +34,19 @@ public class AccountController extends BaseController {
     public Result recharge(@RequestBody RechargeAccountRequest request){
         request.setUserId(getUserId());
         return accountRechargeProtocol.recharge(request);
+    }
+
+    /**
+     * 账户余额查询（当前登录用户）
+     * 完整调用链：
+     * account.vue -> ams-app(AccountController) -> account-bsm-biz-service(AccountProtocolImpl)
+     *   -> AccountServiceImpl -> AccountMapper(selectAccount) -> MyBatis -> tf_b_account 表
+     *
+     * POST /account/balance
+     */
+    @PostMapping("/balance")
+    public Result<AccountDto> balance() {
+        return accountProtocol.selectAccount(getUserId());
     }
 
 }
